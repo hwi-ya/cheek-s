0b2aeb5b31bb3c9c244747ae90fdc61898f6f8a7
때기 <102639871+hwi-ya@users.noreply.github.com>
2025-03-23 21:54:44 +0900

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -50,9 +50,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    // Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.RecentNotes({ limit: 5, showTags: false, showDate: false }),
   ],
 }
 
