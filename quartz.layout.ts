0c882a96262eb90aa4ffad9b35b83c7604582328
때기 <102639871+hwi-ya@users.noreply.github.com>
2025-03-24 16:53:54 +0900

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -15,7 +15,7 @@ export const sharedPageComponents: SharedLayout = {
         categoryId: 'DIC_kwDOONPdF84CoYAX',
         mapping: 'pathname',
         themeUrl: "https://example.com/static/giscus", // corresponds to quartz/static/giscus/
-        lightTheme: "light-theme", // corresponds to light-theme.css in quartz/static/giscus/
+        lightTheme: "dark-theme", // corresponds to light-theme.css in quartz/static/giscus/
         darkTheme: "dark-theme", // corresponds to dark-theme.css quartz/static/giscus/
       }
     }),
