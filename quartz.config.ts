a906769935f774cf37e5e03d95abc284f8f5e029
때기 <102639871+hwi-ya@users.noreply.github.com>
2025-03-25 12:52:04 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "I'm Cheek's",
+    pageTitle: "I 'HATE' cheek's",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
